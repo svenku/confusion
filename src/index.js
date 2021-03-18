cb83fe43b5824dfdec9940ae619f7ed04732c3c8
svenku <sven.kunsing@gmail.com>
2021-03-18 17:59:36 +0200

    Add MenuComponent

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-reportWebVitals();
