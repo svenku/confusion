976a0a694c1f708aa0b555dc087eb9b93e9cd2bb
svenku <sven.kunsing@gmail.com>
2021-03-22 17:12:36 +0200

    Splitting app into Container and Presentational Components.

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   renderComments(comments){
     if (comments != null) {
@@ -12,7 +12,7 @@ class DishDetail extends Component {
         return (
                 <li key={comment.id}>
                   <p>{comment.comment}</p>
-                  <p>-- {comment.author}, {comment.date}</p>
+                  <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
         );
       });
@@ -50,17 +50,19 @@ class DishDetail extends Component {
   }
 
   render() {
-    if (this.props.selectedDish != null) {
+    if (this.props.dish != null) {
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.selectedDish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <ul className="list-unstyled">
-              {this.renderComments(this.props.selectedDish.comments)}
-            </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              {this.renderDish(this.props.dish)}
+            </div>
+            <div className="col-12 col-md-5 m-1">
+              <h4>Comments</h4>
+              <ul className="list-unstyled">
+                {this.renderComments(this.props.dish.comments)}
+              </ul>
+            </div>
           </div>
         </div>
       );
