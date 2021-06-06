29e6ad3fb7f15e309950f4ac28bd860078151d04
svenku <sven.kunsing@gmail.com>
2021-06-07 00:30:18 +0300

    Redux Thunk and Logger

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
+function RenderCard({item, isLoading, errMess}) {
 
-function RenderCard({item}) {
-  return(
+  if (isLoading) {
+    return(
+      <Loading />
+    );
+  }
+
+  else if (errMess) {
+    return (
+      <h4>{errMess}</h4>
+    );
+  }
+
+  else return (
     <Card>
       <CardImg src={item.image} alt={item.name} />
       <CardBody>
@@ -13,15 +26,15 @@ function RenderCard({item}) {
       </CardBody>
     </Card>
   );
-}
 
+}
 
 function Home(props) {
   return(
     <div className="container">
       <div className="row align-items-start">
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.dish}/>
+          <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}/>
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard item={props.promotion}/>
